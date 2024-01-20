52c38474382714e79ca5817b845ffe1d02b1888d
Dulce rojas <dulcerojas698@gmail.com>
2024-01-20 20:44:13 -0300

    renderizando los elementos de DB

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -1,15 +1,25 @@
 import React, { useEffect } from 'react'
-import {useDispath} from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { productAction } from '../redux/actions/productAction'
 
 export const Products = () => {
 
-    const dispatch = useDispath()
+    const dispatch = useDispatch();
+    const products = useSelector(state => state.products && state.products.data); 
+    // const frontend = useSelector(state=>state)
     useEffect(()=>{
+      dispatch(productAction);
+  }, [dispatch]);
 
-        dispatch(productAction)
-    },[])
   return (
-    <div>Products</div>
-  )
-}
+    <div>
+      {products && products.map((elem, i) => (
+        <div key={i}>
+          <p>{elem.name}</p>
+          <p>{elem.Category}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+  
--- client/src/redux/actions/productAction.js
+++ client/src/redux/actions/productAction.js
@@ -1,33 +1,51 @@
-import {getAllProducts,getById} from '../slice/productData'
-import React from 'react'
+// import {getAllProducts,getById} from '../slice/productData'
+// import React from 'react';
+// import axios from 'axios'
 
 
-export const productAction = (dispatch) => {
-  axios("http://localhost:3001/products")
-  .then(res=>dispatch(getAllProducts(res.data.results)))
+// export const productAction = (dispatch) => {
+//   axios("http://localhost:3001/products")
+//   .then(res=>dispatch(getAllProducts(res.data.results)))
 
 
 
-  return async function (dispatch) {
-    var productos = (await axios.get("http://localhost:3001/products"))
-    dispatch(getAllProducts(productos.data))
-    .catch(e=>console.log(e))
+//   return async function (dispatch) {
+//     var productos = (await axios.get("http://localhost:3001/products"))
+//     dispatch(getAllProducts(productos.data))
+//     .catch(e=>console.log(e))
+//   }
+
+
+//     // return async function (dispatch) {
+//     //   try {
+//     //     // Realiza la solicitud con Axios
+//     //     const response = await axios.get("http://localhost:3001/products");
+  
+//     //     // Imprime la respuesta en la consola del navegador
+//     //     console.log(response.data);
+  
+//     //     // Despacha tu acción con los datos si es necesario
+//     //     // dispatch(getAllProducts(response.data.results));
+//     //   } catch (error) {
+//     //     // Maneja los errores, si los hay
+//     //     console.error("Error al obtener productos:", error);
+//     //   }
+//     // };
+//   };
+
+
+  import { getAllProducts } from '../slice/productData';
+import axios from 'axios';
+
+export const productAction = async (dispatch) => {
+  try {
+    // Realiza la solicitud con Axios
+    const response = await axios.get("http://localhost:3001/products");
+    console.log('Data from productAction:', response.data);
+    // Despacha tu acción con los datos si es necesario
+    dispatch(getAllProducts(response.data.results));
+  } catch (error) {
+    // Maneja los errores, si los hay
+    console.error("Error al obtener productos:", error);
   }
-
-
-    // return async function (dispatch) {
-    //   try {
-    //     // Realiza la solicitud con Axios
-    //     const response = await axios.get("http://localhost:3001/products");
-  
-    //     // Imprime la respuesta en la consola del navegador
-    //     console.log(response.data);
-  
-    //     // Despacha tu acción con los datos si es necesario
-    //     // dispatch(getAllProducts(response.data.results));
-    //   } catch (error) {
-    //     // Maneja los errores, si los hay
-    //     console.error("Error al obtener productos:", error);
-    //   }
-    // };
-  };
+};
